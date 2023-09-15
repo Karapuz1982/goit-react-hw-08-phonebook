453a29119f679459622d2088b76be4a6ac9d83f8
Karapuz1982 <121318106+Karapuz1982@users.noreply.github.com>
2023-09-15 23:04:05 +0200

    hw#8

---
+++ src/components/UserMenu/UserMenu.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Box, Button } from '@chakra-ui/react';
+import useAuth from 'hooks/useAuth';
+
+import { useDispatch } from 'react-redux';
+import { logOut } from 'redux/auth/authOperations';
+
+const UserMenu = () => {
+  const dispatch = useDispatch();
+  const { user } = useAuth();
+
+  return (
+    <Box display="flex" gap="30px" ml="auto">
+      <p>{user.name}</p>
+      <Button
+        variant="link"
+        color="inherit"
+        _hover={{ textDecoration: 'none', color: 'purple.600' }}
+        onClick={() => dispatch(logOut())}
+      >
+        Logout
+      </Button>
+    </Box>
+  );
+};
+
+export default UserMenu;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { Provider } from 'react-redux';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { store } from 'redux/store';
+import App from 'components/App';
+import theme from 'utils/theme';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <Provider store={store}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
